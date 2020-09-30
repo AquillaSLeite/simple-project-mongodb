4ab8da91c88e97b5d8219f9f96126296b2e50fcb
Aquilla Silva Leite <aquilla11@hotmail.com>
2020-09-30 20:20:42 -0300

    create project

---
+++ src/test/java/com/example/simpleprojectmongodb/SimpleProjectMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleprojectmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

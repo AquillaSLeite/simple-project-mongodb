7f0a840134888a5a30ee9c8eb4e18fac932cc890
Aquilla Silva Leite <aquilla11@hotmail.com>
2020-10-03 16:18:45 -0300

    Update project and database seeder

--- src/main/java/com/example/simpleprojectmongodb/db/seed/DataBaseSeeder.java
+++ src/main/java/com/example/simpleprojectmongodb/db/seed/DataBaseSeeder.java
@@ -1,5 +1,6 @@
 package com.example.simpleprojectmongodb.db.seed;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.simpleprojectmongodb.model.Post;
 import com.example.simpleprojectmongodb.model.User;
+import com.example.simpleprojectmongodb.model.repository.PostRepository;
 import com.example.simpleprojectmongodb.model.repository.UserRepository;
 import com.example.simpleprojectmongodb.model.service.UserService;
 
@@ -19,16 +22,29 @@ public class DataBaseSeeder implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-
 		userRepository.deleteAll();
-		User user1 = new User("Aquilla Silva", "aquilla@aquilla.com", userService.generateBcrypt("123456"));
-		User user2 = new User("Alejandro Silva", "alejandro@alejandro.com", userService.generateBcrypt("123456"));
-		User user3 = new User("Maria Cicera", "maria@maria.com", userService.generateBcrypt("123456"));
+		postRepository.deleteAll();
+
+		User user1 = new User(null, "Aquilla Silva", "aquilla@aquilla.com", "123456");
+		User user2 = new User(null, "Alejandro Silva", "alejandro@alejandro.com","123456");
+		User user3 = new User(null, "Maria Cicera", "maria@maria.com", "123456");
 
 		List<User> users = userRepository.saveAll(Arrays.asList(user1, user2, user3));
-		users.forEach(System.out::println);
+		
+		for(User user : users) {
+			Post post1 = new Post(null, Instant.now(), user.getName() + " - title1", "body1", user.getId());
+			Post post2 = new Post(null, Instant.now(), user.getName() + " - title2", "body2", user.getId());
+			Post post3 = new Post(null, Instant.now(), user.getName() + " - title3", "body3", user.getId());
+			postRepository.saveAll(Arrays.asList(post1, post2, post3));
+			
+			user.getPosts().addAll(Arrays.asList(post1, post2, post3));
+			
+			userRepository.save(user);
+		}
 	}
 }

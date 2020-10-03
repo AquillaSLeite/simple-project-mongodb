f350119170e50e2e53bdfb7b50d01e5133cf7e93
Aquilla Silva Leite <aquilla11@hotmail.com>
2020-10-03 16:16:22 -0300

    excluding implementation of generic classes

--- src/main/java/com/example/simpleprojectmongodb/model/generic/GenericDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.simpleprojectmongodb.model.generic;
-
-public interface GenericDTO<T extends GenericModel>{
-	GenericDTO<T> generateDto(T obj);
-}
--- src/main/java/com/example/simpleprojectmongodb/model/generic/GenericRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simpleprojectmongodb.model.generic;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenericRepository<T extends GenericModel> extends MongoRepository<T, String>{
-
-}
